1f69964b9eb34ae5030c00fd6d93d87995439eef
Zeck-Garcia <henriquegarcia-hg@hotmail.com>
2022-09-30 23:18:30 -0300

    fatora cod

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const btnRight = window.document.querySelector('.partner-brand-btn.-right')
 const btnLeft = window.document.querySelector('.partner-brand-btn.-left')
 const elements = window.document.querySelector('.partner-band-carousel-elements')
-let move = 100
+let move = 0
 
 
 btnRight.addEventListener('click', function(){
