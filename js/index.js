23aaf80ab633c6a89fa29f8362a93747ca74ed94
Zeck-Garcia <henriquegarcia-hg@hotmail.com>
2022-10-12 14:00:28 -0300

    create menu hamburguer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// btn pass home
 const btnRight = window.document.querySelector('.partner-brand-btn.-right')
 const btnLeft = window.document.querySelector('.partner-brand-btn.-left')
 const elements = window.document.querySelector('.partner-band-carousel-elements')
@@ -12,4 +13,18 @@ btnRight.addEventListener('click', function(){
 btnLeft.addEventListener('click', function(){
     move = move - 100;
     elements.style = `transform: translateX(${move}px);`
+})
+
+
+// menu mobile
+const menu = window.document.querySelector('.menu')
+const btnOpen = window.document.querySelector('.menu-hamburguer')
+const btnClose = window.document.querySelector('.close')
+
+btnOpen.addEventListener('click', function(){
+    menu.classList.add('menu-open'); 
+})
+
+btnClose.addEventListener('click', function(){
+    menu.classList.remove('menu-open'); 
 })
