b8495e25ff97bd19d728e66e190e5981906f3394
Zeck-Garcia <henriquegarcia-hg@hotmail.com>
2022-10-21 21:28:28 -0300

    edit event js

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,13 @@ const menu = window.document.querySelector('.menu')
 const btnOpen = window.document.querySelector('.menu-hamburguer')
 const btnClose = window.document.querySelector('.close')
 
-btnOpen.addEventListener('click', function(){
+btnOpen.addEventListener('click', function(event){
+    event.preventDefault()
     menu.classList.add('menu-open'); 
     console.log('ae');
 })
 
-btnClose.addEventListener('click', function(){
+btnClose.addEventListener('click', function(event){
+    event.preventDefault()
     menu.classList.remove('menu-open'); 
 })
